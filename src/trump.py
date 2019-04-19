3ccfe924f1875a03f011075f4515417c15ddd16a
Alexander Steeb <alexander.steeb@gmx.de>
2019-04-19 22:59:15 +0200

    Changed to Bible data and implemented first version of LDA for old and new testament

--- src/trump.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('trump.json', encoding="utf8") as json_file:  
-    data = json.load(json_file)
-    
-for tweet in range(0, len(data)):
-    print(data[tweet]["created_at"])
-    print(data[tweet]["text"])
